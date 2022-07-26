3e9a19ce63d5e0be932ee387ee583014c17a3f65
Alicefortes <alicefortsp@gmail.com>
2022-07-26 19:51:53 -0300

    ajustes

--- functions.php
+++ functions.php
@@ -26,8 +26,8 @@
     function css_files() {
         wp_enqueue_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css');
         wp_enqueue_style('poppins', 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap');
+        wp_enqueue_style('header', get_template_directory_uri().'/assets/css/header.css');
         wp_enqueue_style('style', get_template_directory_uri().'/assets/css/style.css');
-        wp_enqueue_style('header', get_template_directory_uri() . '/assets/css/header.css');
         wp_enqueue_style('footer', get_template_directory_uri().'/assets/css/footer.css');
        
         if ( is_front_page() == true ) {         // só vai carregar se estiver na front-page         
--- header.php
+++ header.php
@@ -9,6 +9,7 @@
     <?php wp_head(); ?>
 </head>
 <body>
+
 <nav class="navbar sticky-top navbar-expand-lg">
         <div class="container p-0 ">
             <div class="logo p-0 m-0">
@@ -21,16 +22,25 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse justify-content-end  menu" id="navbarNav" >
-              <!-- Criar menus no wordpress com o nome "main_nav" para o menu aparecer -->
-              <?php wp_nav_menu(array(
-                    'menu' => 'main_nav',
-                    'menu_class' => 'navbar-nav',
-                    'theme_location' => 'primary',
-                    'container' => 'false',
-                    // 'walker' => new WP_Bootstrap_Navwalker()
-                    )
-                    );?>
-              
+            <ul class="navbar-nav mb-2 mb-lg-0 d-flex align-items-center">
+                        <li class="nav-item">
+                            <a class="nav-link <?php if(is_front_page()) { echo 'ativo';} ?>" aria-current="home" href="<?php echo get_home_url(); ?>"><h4 class">Home</h4></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link <?php if(is_page('sobre') == true ) { echo 'ativo';} ?>" aria-current="sobre" href="<?php echo get_home_url(); ?>/sobre"><h4 class">Quem Somos</h4></a>
+                        </li>
+                        
+                        <li class="nav-item">
+                            <a class="nav-link <?php if(is_page('servicos') == true ) { echo 'ativo';} ?>" aria-current="serviços" href="<?php echo get_home_url(); ?>/servicos"><h4 class">Serviços</h4></a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link <?php if(is_category() == true || is_single()) { echo 'ativo';} ?>" aria-current="blog" href="<?php echo get_home_url(); ?>/category/geral"><h4>Blog</h4></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link <?php if(is_page('contato') == true ) { echo 'ativo';} ?>" aria-current="home" href="<?php echo get_home_url(); ?>/contato"><h4 class">Contato</h4></a>
+                        </li>
+                    </ul>
                 
             </div>
         </div>
